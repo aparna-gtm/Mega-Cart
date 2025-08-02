1b0d3c9a8557652408bd36223dbd03f9a0b4783f
aparna-gtm <aparnagautam3323@gmail.com>
2025-08-03 01:38:31 +0530

    new commit

--- app.js
+++ app.js
@@ -142,6 +142,8 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(8080,()=>{
-    console.log("server connceted at port 8080");
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
